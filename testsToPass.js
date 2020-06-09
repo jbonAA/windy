0bc9f2f1b330c453a38a2269a799085187f1d5a6
Jess Bon <j_bon@comcast.net>
2020-06-09 14:07:46 -0700

    Functionality to Test

---
+++ testsToPass.js
@@ -0,0 +1,12 @@
+// 1. application should render map of sanFran by default
+//    application should render forecast of SF by default
+
+// 2. on button Click
+//     extract lng/lat of four corners and map center
+//     extract sizing of map component for scale when drawing lines
+//     create new wind object with data
+//     canvas applied and sized over map component
+//     run through functions
+//         api call to pull forecast from center
+//         probably want a preloaded state in my map component
+
